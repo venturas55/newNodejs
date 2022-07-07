d5ca6855af820d56f87b2f87bf9386b12b4b38cb
Adrian de haro <venturas55@hotmail.com>
2022-07-07 14:12:08 +0200

    Iniciamos nodejs app

---
+++ src/routes/api.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const helpers = require('../lib/helpers');
+
+router.get('/api/prueba',async (req,res)=>{
+    //PARA GESTIONAR UNA API
+    res.redirect(json);
+});
+
+module.exports = router;
